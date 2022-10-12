cc65848897effe8e2fddf75d7ddd8545ab836182
Aminu Rabiu <arabiu033@gmail.com>
2022-10-12 23:57:41 +0100

    Add a js file responsible for the game creation in the API

---
+++ src/modules/game.js
@@ -0,0 +1,15 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'
+let response = await fetch(baseUrl, {
+method: 'POST',
+headers: {
+  'Content-Type': 'application/json'
+},
+body: JSON.stringify({ 
+  "name": "unpocoloco" 
+})
+});
+
+const endpoint = await response.json();
+const gameUrl = baseUrl.concat(endpoint.result.split(' ')[3], '/scores/');
+
+export default gameUrl;
