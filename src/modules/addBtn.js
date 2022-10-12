79f5ee99535a3eacb09b15d432cdf96b14d5309f
Aminu Rabiu <arabiu033@gmail.com>
2022-10-12 23:56:10 +0100

    Add a js file responsible for the scores update

---
+++ src/modules/addBtn.js
@@ -0,0 +1,34 @@
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+let add, addRes;
+
+const addScore = async (e, gameUrl) => {
+  e.preventDefault()
+
+  if (name.value == '' || score.value == '') {
+    return;
+  } else if (!score.value.match(/^[0-9]+$/)) {
+    score.value = ''
+    score.classList.add('red');
+    score.setAttribute('placeholder', 'Please enter a valid score')
+    return
+  }
+
+  add = await fetch(gameUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ 
+    "user": name.value,
+    "score": +score.value
+  })
+  });
+  
+  addRes = await add.json();
+  score.classList.remove('red');
+  score.setAttribute('placeholder', 'Your score')
+  score.value = name.value = '';
+}
+
+export default addScore;
