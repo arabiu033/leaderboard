733b0258f1c1d3993613498b656d87990bb25495
Aminu Rabiu <arabiu033@gmail.com>
2022-10-12 23:55:35 +0100

    Add the js file for the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import gameUrl from './modules/game';
+import addScore from './modules/addBtn';
+import resetBtn from './modules/resteBtn';
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', (e) => addScore(e, gameUrl));
+
+
+const reset = document.getElementById('reset-btn');
+reset.addEventListener('click', () => resetBtn(gameUrl));
