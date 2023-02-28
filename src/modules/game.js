7fbb98b141b61d24ce2d410a2ee2ea5fc054df84
Aminu Rabiu <arabiu033@gmail.com>
2023-02-28 12:23:06 +0100

    Remove comments

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,16 +1,4 @@
 const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-// const response = await fetch(baseUrl, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({
-//     name: 'unpocoloco',
-//   }),
-// });
-
-// const endpoint = await response.json();
-// alert(endpoint.result);
 const gameUrl = baseUrl.concat('NXVaV2jg5gShsu30JPu3', '/scores/');
 
 export default gameUrl;
